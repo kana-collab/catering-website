f29074fb1c8a759e687f272ea92c8d22d8266588
Harshil <zip5737@gmail.com>
2024-12-30 16:05:35 -0500

    try1

--- cateringWeb/cateringWeb/urls.py
+++ cateringWeb/cateringWeb/urls.py
@@ -34,6 +34,6 @@ urlpatterns = [
     #path('', CheckAdate, name='CheckADate'),
 ]
 
-if settings.DEBUG is False:
+if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

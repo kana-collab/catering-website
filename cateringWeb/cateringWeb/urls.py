5cd0f82d91b38443ff98dd03fb2f05f7726fed66
Harshil <zip5737@gmail.com>
2024-12-30 15:35:56 -0500

    corrected the urls.py again

--- cateringWeb/cateringWeb/urls.py
+++ cateringWeb/cateringWeb/urls.py
@@ -34,6 +34,6 @@ urlpatterns = [
     #path('', CheckAdate, name='CheckADate'),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG is False:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

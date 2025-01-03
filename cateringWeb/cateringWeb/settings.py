a394eb3a917f4fe41ef5e8aabf2cb8dcedbb28e9
Harshil <zip5737@gmail.com>
2025-01-03 14:09:08 -0500

    updating

--- cateringWeb/cateringWeb/settings.py
+++ cateringWeb/cateringWeb/settings.py
@@ -123,15 +123,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR , "media")
+    os.path.join(BASE_DIR, "cateringWeb/static"),
+    os.path.join(BASE_DIR , "cateringWeb/media")
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR , "media")
+MEDIA_ROOT = os.path.join(BASE_DIR , "cateringWeb/media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

477cb3d25a425a4f0b3dfec67394812bda9d7ea2
Harshil <zip5737@gmail.com>
2025-01-05 15:56:45 -0500

    updating21

--- cateringWeb/cateringWeb/settings.py
+++ cateringWeb/cateringWeb/settings.py
@@ -130,8 +130,8 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR , "media")
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR , "static/media")
 
 
 
